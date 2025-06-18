4b1c9b7f3d19c8b3c34b1e2521d4749e665a6cb9
nksazonov <nsazonov@openware.com>
2025-06-18 11:36:27 +0300

    fix(formula): update formula url

--- Formula/chain-rpc.rb
+++ Formula/chain-rpc.rb
@@ -1,7 +1,7 @@
 class ChainRpc < Formula
   desc "Fast CLI tool for finding public RPC endpoints for blockchain networks"
   homepage "https://github.com/nksazonov/chain-rpc"
-  url "https://github.com/nksazonov/chain-rpc/archive/v0.1.0.tar.gz"
+  url "https://github.com/nksazonov/chain-rpc/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
 
